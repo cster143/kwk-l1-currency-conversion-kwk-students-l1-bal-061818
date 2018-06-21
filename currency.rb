3f1a9d7c58712b51c9a7f591eaec6826b4336578
cster143 <cster143@gmail.com>
2018-06-21 15:37:47 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,4 +1,9 @@
-# Write your cod here
+# a dollar amount as an argument, and return the equivalent value in Euros
+# 1.00 to 1.20
 def usd_to_eur
 
+
+
+# an amount in Euros as an argument, and return the equivalent value in US Dollars.
+# 1.20 to 1.00
 def eur_to_usd
